ace93e91ba38ddcef56756a2cbf64ed0496c27f2
WaylandSmith129 <117129029+WaylandSmith129@users.noreply.github.com>
2022-12-28 14:42:39 +1300

    Edit file to complete task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let combine = '';
+    if(num > -1){
+        for(i = 0; i < num; i++){
+            combine += word;
+        }
+    }else{
+        combine = 'ERROR';
+    }
+    
+    return combine;
 };
 
 // Do not edit below this line
