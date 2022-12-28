56169563b0fe014e5c0fdda7a6b9ab29c953b148
WaylandSmith129 <117129029+WaylandSmith129@users.noreply.github.com>
2022-12-29 11:34:27 +1300

    Edit script to reverse input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newArray = word.split("");
+    let reverse = [];
+    let j = 0;
+    for(let i = newArray.length -1; i >= 0; i--){
+        reverse[j] = newArray[i];
+        j++;
+    }
+    return reverse.join("");
 };
 
 // Do not edit below this line
